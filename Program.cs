77400efd3fcd2bafecea6d5e6891b0d769d4afdb
Ubuntu <ubuntu@ip-172-31-6-44.us-west-2.compute.internal>
2017-01-17 11:58:45 +0000

    Code

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloWorldCore
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello! This is my first dotnet Core App");
+            Console.ReadLine();
+        }
+    }
+}
