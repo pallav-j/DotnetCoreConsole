b10597b62db6634795629da8a3e668e516cf6d1f
Pallav Jain <pallavj@gmail.com>
2017-01-20 23:48:58 +0530

    This is version 4

--- Program.cs
+++ Program.cs
@@ -10,6 +10,7 @@ namespace HelloWorldCore
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello! This is my first dotnet Core App with CI using Docker, CodeBuild and CodeDeploy. Yay!");
+            Console.WriteLine("This is Line 2");
             Console.ReadLine();
         }
     }
